07be0dd7ed4a7024d6536fc20fdd8393f90a9509
Enock Kalonji <tshims.enock31@gmail.com>
2022-07-20 14:53:08 -0400

    adding Project2

---
+++ Project2_GuessTheNumber/src/com/company/Main.java
@@ -0,0 +1,54 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while (true){
+            play(1, 20);
+            String response;
+
+            System.out.println("Would you like to play again? (y or n)");
+            response = sc.nextLine();
+            if (response.equals("y")) {
+                System.out.println("Welcome back!\n");
+            }
+            if (response.equals("n")) {
+                System.out.println("Goodbye!!!");
+                break;
+            }
+        }
+    }
+
+    public static void play(int min, int max){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Hello! What is your name?");
+        String name = input.nextLine();
+        System.out.println("well, " + "\033[0;1m" + name + ", I am thinking of a number between 1 and 20.\n" +
+                "You have six(6) trials\n");
+
+
+        int random = (int) Math.floor(Math.random() * (max - min + 1) + min);
+        int guessCount = 0;
+
+        while(guessCount <= 5) {
+            System.out.println("take a guess.");
+            int guess = input.nextInt();
+
+            if (guess > random) {
+                System.out.println("Your guess is to high.");
+                guessCount++;
+                continue;
+            } else if (guess < random) {
+                System.out.println("Your guess is to low");
+                guessCount++;
+                continue;
+            }
+            guessCount++;
+            System.out.println("Good job, " + name + "! You guessed my number in " + guessCount + "\n");
+            break;
+        }
+    }
+}
